c97f390dd0e514a168d55268e4ca9d6198db0718
Muhammad-Sameed-Ansari <52503336+Muhammad-Sameed-Ansari@users.noreply.github.com>
2022-09-13 23:16:25 +0500

    Initial Commit

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Enter Your Phone Number</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
